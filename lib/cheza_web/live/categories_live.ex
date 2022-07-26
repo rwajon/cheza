f034900c51bfa06d407659d08590d786b0e32688
Rwabahizi Jonathan <jonathanrwabahizi@gmail.com>
2022-07-26 09:50:29 +0200

    create categories page

--- lib/cheza_web/live/categories_live.ex
+++ lib/cheza_web/live/categories_live.ex
@@ -2,6 +2,7 @@ defmodule ChezaWeb.CategoriesLive do
   use Phoenix.LiveView
 
   def mount(_params, _session, socket) do
+    socket = assign(socket, :categories, Cheza.Helpers.Categories.get_categories())
     {:ok, socket}
   end
 
